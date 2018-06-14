13254012a318d48738f85bedee20336ec9932c28
USER <USER@DESKTOP-B6S2AVF>
2018-06-14 16:23:20 +0530

    Books Get api

--- RestDemoApp/src/main/java/com/learnings/rest/restdemoapp/resource/BookResource.java
+++ RestDemoApp/src/main/java/com/learnings/rest/restdemoapp/resource/BookResource.java
@@ -2,6 +2,7 @@ package com.learnings.rest.restdemoapp.resource;
 
 import java.util.List;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -10,18 +11,24 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.slf4j.Logger;
+
 import com.learnings.rest.restdemoapp.model.Book;
 import com.learnings.rest.restdemoapp.service.BookService;
 
 @Path("books")
 public class BookResource {
 
-	BookService bookService = new BookService();
+	@Inject
+	Logger logger;
+	@Inject
+	BookService bookService;// = new BookService();
 	
 	@GET
 	@Path("/ping")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String ping() {
+		logger.info("Books Ping");
 		return "Books PING Success!!!";
 		/*http://localhost:8080/RestDemoApp/messages/ping
 */	
@@ -35,9 +42,10 @@ public class BookResource {
 	}
 	
 	@GET
-	@Path("{bookId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{bookId}")
 	public Response getBook(@PathParam("bookId") Long id) {
-		Book book = bookService.getBook(id); 
+		Book book = bookService.getBook(id);
 		return Response.status(Status.OK).entity(book).build();
 		
 	}
--- RestDemoApp/src/main/java/com/learnings/rest/restdemoapp/service/BookService.java
+++ RestDemoApp/src/main/java/com/learnings/rest/restdemoapp/service/BookService.java
@@ -5,12 +5,24 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.ext.Provider;
+
+import org.slf4j.Logger;
+
 import com.learnings.rest.restdemoapp.model.Book;
 
+
+@ApplicationScoped
 public class BookService {
-	private Map<Long,Book> books = new HashMap<Long, Book>();
+/*	@Inject
+	Logger logger;
+*/	private Map<Long,Book> books = new HashMap<Long, Book>();
 	
 	public BookService() {
+//		logger.info("Book Service - Constructor");
+		System.out.println("Book Service Constructor");
 		 for (Long i=1L; i<=10; i++) {
 			 Book m = new Book(i,"Book Title - "+i, "Subbu-"+i);
 			 books.put(i, m);
@@ -19,9 +31,16 @@ public class BookService {
 	}
 	
 	public List<Book> getBooks(){
+		System.out.println("Book Service - Get Books");
 		return new ArrayList<Book> (books.values());
 	}
 	public Book getBook(Long id) {
-		return books.get(id);
+		System.out.println("Book Service - Get Book for id"+id );
+		Book b = books.get(id);
+/*		if (b != null) 
+				logger.info(b.getAuthor());
+		else
+			logger.info("GetBook - No Object Found");
+*/		return b;
 	}
 }
