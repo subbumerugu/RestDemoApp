f445be2e7989a5cb0568dc73d1598cf8425df067
USER <USER@DESKTOP-B6S2AVF>
2018-06-14 14:00:34 +0530

    rest flow test

---
+++ RestDemoApp/src/main/java/com/learnings/rest/restdemoapp/application/DemoApplication.java
@@ -0,0 +1,9 @@
+package com.learnings.rest.restdemoapp.application;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class DemoApplication extends Application{
+
+}
